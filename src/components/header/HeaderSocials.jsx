091c7653d0276ca14ca93f7d1f69b0e872fb7e67
katygil7 <gil.katherine22@gmail.com>
2022-11-07 12:56:51 -0600

    Fixed Json file. Added react icons

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import {BsLinkedin} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
-    <div>HeaderSocials</div>
+    <div className='header_socials'>
+        <a href='https://linkedin.com/in/katherinegil22/' target={_blank}><BsLinkedin/></a>
+        <a href='https://github.com/katygil7' target={_blank}></a>
+    </div>
   )
 }
 
