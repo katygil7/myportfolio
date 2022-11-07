4d44825e56864fed45ba0b0be72ec5b6a43927ce
katygil7 <gil.katherine22@gmail.com>
2022-11-06 23:58:10 -0600

    added my cv, working on header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
-function Header() {
-  return (
-    <div>Header</div>
+const Header = () => {
+  return(
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Katherine Gil</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+      </div>
+    </header>
   )
 }
 
