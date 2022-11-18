43717f353c175aeb1766ba496439e2420b9a6746
katygil7 <gil.katherine22@gmail.com>
2022-11-18 16:37:44 -0500

    added project pics and links to github

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assests/IMG1.png'
+import IMG2 from '../../assests/IMG2.png'
+import IMG3 from '../../assests/IMG3.png'
+import IMG4 from '../../assests/IMG4.png'
+
 
 
 function Portfolio() {
@@ -9,11 +14,39 @@ function Portfolio() {
       <h2>Portfolio</h2>
 
       <div className='container portfolio_container'>
-        <article className='portfolio_item'>
-          <div className='portfolio_item-image'></div>
-          <h3>This is a portfolio item title</h3>
 
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+          <img src={IMG4} alt = ""/>
+          </div>
+          <h3>iPlant</h3>
+          <a href='https://github.com/i-Plant/iPlant' className='btn' target='_blank' rel="noopener noreferrer">Github</a>
         </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+            <img src={IMG3} alt = ""/>
+          </div>
+          <h3>GreenCup Films</h3>
+          <a href='https://github.com/GreenCup-Films/movies-kat-louie' className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+          <img src={IMG2} alt = ""/>
+          </div>
+          <h3>WeatherMap</h3>
+          <a href='https://github.com/katygil7/codeup-web-exercises/blob/main/weather_map.html' className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+        </article>
+
+        <article className='portfolio_item'>
+          <div className='portfolio_item-image'>
+          <img src={IMG1} alt = ""/>
+          </div>
+          <h3>Coffee</h3>
+          <a href='https://github.com/katygil7/coffee-katherine-jacob' className='btn' target='_blank' rel="noopener noreferrer">Github</a>
+        </article>
+
 
       </div>
 
