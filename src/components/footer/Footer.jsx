715c36dc06587e977940f399b29175cb1a593923
katygil7 <gil.katherine22@gmail.com>
2022-12-01 20:13:13 -0500

    css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,18 +4,16 @@ import './footer.css'
 
 const Footer = () => {
   return (
-   <Footer>
-    <a href='home' className='footer_logo'>Katherine Gil</a>
+   <footer>
+    <a href='/' className='footer_logo'>Katherine Gil</a>
 
-    <ul>
-      <li><a href='home'>Home</a></li>
-      <li><a href='about'>About</a></li>
-      <li><a href='experience'>Experience</a></li>
-      <li><a href='services'>Services</a></li>
-      <li><a href='portfolio'>Portfolio</a></li>
-      <li><a href='contact'>Contact</a></li>
+    <ul className='permalinks'>
+      <li><a href='#about'>About</a></li>
+      <li><a href='#experience'>Experience</a></li>
+      <li><a href='#portfolio'>Portfolio</a></li>
+      <li><a href='#contact'>Contact</a></li>
     </ul>
-   </Footer>
+   </footer>
   )
 }
 
