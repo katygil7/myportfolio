32ca43a119c25bda9df38e733e21d8a3a9f03bbe
katygil7 <gil.katherine22@gmail.com>
2022-12-07 11:56:39 -0500

    Updates

---
+++ precache-manifest.659fcf4f7f3593982cb29cbad7724992.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8c953b3cff923a1976b",
+    "url": "/myportfolio/static/css/main.eb92f169.chunk.css"
+  },
+  {
+    "revision": "d8c953b3cff923a1976b",
+    "url": "/myportfolio/static/js/main.d8c953b3.chunk.js"
+  },
+  {
+    "revision": "be2c24cf88514f2594ec",
+    "url": "/myportfolio/static/js/1.be2c24cf.chunk.js"
+  },
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "20ce6f330695d45c4ad94c6457dd2067",
+    "url": "/myportfolio/static/media/cv.20ce6f33.pdf"
+  },
+  {
+    "revision": "3d33eca879b2b614300fa9edc505579f",
+    "url": "/myportfolio/static/media/me.3d33eca8.PNG"
+  },
+  {
+    "revision": "457eaf5bcb6a75632dd4da074e61bfae",
+    "url": "/myportfolio/static/media/me_about.457eaf5b.png"
+  },
+  {
+    "revision": "3c0720f0c3434342aabfe1d3491fdef7",
+    "url": "/myportfolio/static/media/IMG1.3c0720f0.png"
+  },
+  {
+    "revision": "933b8ca1ec07a2b4f9b54e9e7e5f07de",
+    "url": "/myportfolio/static/media/IMG2.933b8ca1.png"
+  },
+  {
+    "revision": "8f1ddb1807dea46b7fa3b0af7a159e50",
+    "url": "/myportfolio/static/media/IMG3.8f1ddb18.png"
+  },
+  {
+    "revision": "3d7c4b8d10dc7b72ece1f3026f566463",
+    "url": "/myportfolio/static/media/IMG4.3d7c4b8d.png"
+  },
+  {
+    "revision": "974b8b310775f5ca2001c70dfb7c1f5d",
+    "url": "/myportfolio/index.html"
+  }
+];
