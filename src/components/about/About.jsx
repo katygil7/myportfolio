c88c53a63c2d08af14218b664eb7c26b1669d72a
katygil7 <gil.katherine22@gmail.com>
2023-01-11 17:43:41 -0500

    updated resume

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './about.css'
-import ME from '../../assests/me_about.png'
+// import ME from '../../assests/me.png'
 import {FaAward} from 'react-icons/fa'
 import {FaUserGraduate} from 'react-icons/fa'
 import {CgUserlane} from 'react-icons/cg'
@@ -12,14 +12,20 @@ const About = () => {
     <h2>About Me</h2>
 
     <div className='container about_container'>
-      <div className='about_me'>
+      {/* <div className='about_me'>
         <div className='about_me-image'>
           <img src={ME} alt='About Me' />
         </div>
-      </div>
+      </div> */}
       
       <div className='about_content'>
         <div className='about_cards'>
+
+        <article className='about_card'>
+            <FaUserGraduate className='about_icon' />
+            <h5>Education</h5>
+            <small>CodeUp 2022, William Peace University</small>
+          </article>
 
           <article className='about_card'>
             <FaAward className='about_icon' />
@@ -33,14 +39,10 @@ const About = () => {
             <small>Crossfit Athlete, Mountain Biking, Personal Growth Enthusiastic</small>
           </article>
 
-          <article className='about_card'>
-            <FaUserGraduate className='about_icon' />
-            <h5>Education</h5>
-            <small>CodeUp 2022, William Peace University</small>
-          </article>
+        
         </div>
         <p>
-        Software developer and Army Veteran with technical and management experience as a logistics specialist. Enthusiastic and eager to contribute to team success through hard work, attention to detail and excellent organizational and communication skills. Utilizing full-stack languages enhanced by the appropriate development frameworks in order to produce efficient code that maximizes performance while maintaining existing software functionality.
+        Software developer and Army Veteran with technical and management experience as a logistics specialist. Utilizing full-stack languages enhanced by the appropriate development frameworks in order to produce efficient code that maximizes performance while maintaining existing software functionality.
         </p>
 
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
